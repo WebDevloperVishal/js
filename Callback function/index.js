db4181cd1abb9ac607a36d8070b8246b7a332453
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-07-30 21:02:51 +0530

    first commit

---
+++ Callback function/index.js
@@ -0,0 +1,34 @@
+let greet = (name, callback) => {
+    console.log('Hello' + name);
+    callback();
+}
+
+let Saybye = () => {
+    console.log('Bye');
+}
+
+greet('Aryan', Saybye);
+
+// Callback with Sync
+
+setTimeout(() => {
+    console.log('Aryan');
+}, 2000);
+
+setTimeout(() => {
+    console.log('Aryan');
+}, 1000);
+
+console.log('Ayan 2');
+
+// Callback hell
+
+setTimeout(() => {
+    console.log('Aryan 1');
+    setTimeout(() => {
+        console.log('Aryan 2');
+        setTimeout(() => {
+            console.log('Aryan 3');
+        }, 1000)
+    }, 1000)
+}, 1000)
---
+++ Sync & Async/index.js
@@ -0,0 +1,13 @@
+// synchronnus
+console.log('Start');
+console.log('This will run');
+console.log('End');
+
+// <--------------------------------->
+// asynchronnus
+console.log('Aryan');
+
+setTimeout(()=>{
+    console.log('Yash');
+})
+console.log('Vedant');
