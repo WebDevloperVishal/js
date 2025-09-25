b486c523580d9297e67a3cbd07a4ef372b9d480d
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-09-25 12:05:51 +0530

    OOPs

---
+++ OOP in JS/index.js
@@ -0,0 +1,16 @@
+class Car{
+    brand;
+    model;
+
+    displayInfo(){
+        console.log(`Car Brand = ${brand} and Model = ${model}`);
+        
+    }
+
+}
+
+let n = 10;
+const Car = new Car();
+c.brand = "Toyota";
+c.model = "Fortuner";
+c.displayInfo();
