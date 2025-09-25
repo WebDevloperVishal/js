df0c16337e2a1c86c41dbda38e8474d4d68f8a95
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-09-25 12:48:17 +0530

    OOPs

---
+++ OOP in JS/Abstraction/index.js
@@ -0,0 +1,13 @@
+class CoffeeMachine{
+    startMachine(){
+        console.log("Starting the Machine");
+    }
+    brewCoffee(){
+        console.log("Brawing Coffee");
+    }
+    makeCoffee(){
+        this.startMachine();
+        this.brewCoffee();
+        console.log('Your Coffee is Ready');
+    }
+}
