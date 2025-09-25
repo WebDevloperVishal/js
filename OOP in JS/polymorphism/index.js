421affc9ce0758f4c567ab8a535593e0cba3d053
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-09-25 13:00:36 +0530

    OOPs

--- OOP in JS/polymorphism/index.js
+++ OOP in JS/polymorphism/index.js
@@ -1,3 +1,11 @@
 class Bank{
-    
+    roi(){
+        console.log('RBI Interest rate = 4.2');
+    }
+}
+
+class SBI extends Bank{
+    roi(){
+        console.log('RBI Interest rate = 7.2');
+    }
 }
