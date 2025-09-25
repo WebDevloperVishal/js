82d52d8c81d4b959c7620aa86b88033dd5c36203
WebDevloperVisha <vishalsanam2024@gmail.com>
2025-09-25 12:21:37 +0530

    OOPs

--- OOP in JS/constructor/index.js
+++ OOP in JS/constructor/index.js
@@ -1,26 +1,26 @@
-class Car{
+class Car {
     brand;
     model;
 
-    constructor(brand,model){
+    constructor(brand, model) {
         this.brand = brand;
         this.model = model;
     }
 
-    displayInfo(){
+    displayInfo() {
         console.log(`Car Brand = ${this.brand} and Model = ${this.model}`);
-        
+
     }
 
 }
 
 let n = 10;
-const c = new Car("Toyota","Fortuner");
+const c = new Car("Toyota", "Fortuner");
 // c.brand = "Toyota";
 // c.model = "Fortuner";
 c.displayInfo();
 
-const c2 = new Car("BMW","BMW M4 - Serices")
-    // c2.brand = "BMW";
-    // c2.model = "BMW M4 - Serices"
-    c2.displayInfo();
+const c2 = new Car("BMW", "BMW M4 - Serices")
+// c2.brand = "BMW";
+// c2.model = "BMW M4 - Serices"
+c2.displayInfo();
